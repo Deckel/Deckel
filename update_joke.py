59e1aae248a6ee8843f18ec7493ff50b987ff9fd
Deckel de Lange <deckel.delange@gmail.com>
2024-10-22 22:40:00 +0100

    Update update_joke.py

--- update_joke.py
+++ update_joke.py
@@ -10,6 +10,7 @@ joke = pyjokes.get_joke()
 # Replace the joke line
 for i, line in enumerate(content):
     if '**Enjoy a daily auto-generated joke**:' in line:
+        print(f"Found like at {i}")
         content[i] = f'**Enjoy a daily auto-generated joke**: {joke}'
 
 # Write back to the README file
