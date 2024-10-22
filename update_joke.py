343e007516b9f921ad475ac6170e027fa9888ec9
Deckel de Lange <deckel.delange@gmail.com>
2024-10-22 22:45:45 +0100

    Update update_joke.py

--- update_joke.py
+++ update_joke.py
@@ -1,4 +1,11 @@
 import pyjokes
+from datetime import datetime
+
+# Get the current date
+current_date = datetime.now()
+
+# Format the date as 'yyyy-mm-dd'
+formatted_date = current_date.strftime("%Y-%m-%d")
 
 # Load the README file
 with open('README.md', 'r') as file:
@@ -11,7 +18,7 @@ joke = pyjokes.get_joke()
 for i, line in enumerate(content):
     if '**Enjoy a daily auto-generated joke**:' in line:
         print(f"Found like at {i}, updating with the joke {joke}")
-        content[i] = f'**Enjoy a daily auto-generated joke**: <p> {joke} </p>'
+        content[i] = f'**Enjoy a daily auto-generated joke**: (last updated at: {formatted_date}) <p> {joke} </p>'
 
 # Write back to the README file
 with open('README.md', 'w') as file:
